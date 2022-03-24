243c8d98fe1e499f72caa9e508574beb1e945f53
Jai Kumar <kjai1903@gmail.com>
2022-03-23 21:01:26 -0400

    uploading changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'apollo-angular-example';
+export class AppComponent implements OnInit, OnDestroy{
+  
+  title = 'Apollo GraphQL Angular Example';
+
+  private GET_RATES = gql `
+  {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+  `
+
+  constructor(private apollo: Apollo){
+
+  }
+    ngOnDestroy(): void {
+        //throw new Error('Method not implemented.');
+    }
+
+  ngOnInit(): void {
+
+  }
+
+  btnGetCurrencyClick(){
+    this.getCurrencyRates()
+  }
+
+  getCurrencyRates(){
+    this.apollo.watchQuery({
+      query: this.GET_RATES
+    })
+    .valueChanges.subscribe(result => {
+      console.log(result.data)
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Apollo Required
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApolloModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://48p1r2roz4.sse.codesandbox.io',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
